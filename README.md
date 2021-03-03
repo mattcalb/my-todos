52f67b95718bdd0dadf92998efe3e9dee7503be7
mattcalb <mattcalb2002@gmail.com>
2021-03-03 12:25:18 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# What is it?
+A minimalist todo app, where the user can add, delete and mark as completed a todo. The todos are saved in the localStorage, making it possible to the user reload and close the page without losing the todos. 
+# Wich technologies were used?
+- Vanilla HTML, CSS and JavaScript
+# How to use it?
+You can access the app here: [https://mattcalb.github.io/my-todos/](https://mattcalb.github.io/my-todos/)
---
+++ js/index.js
@@ -0,0 +1,87 @@
+var todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+const form = document.querySelector("form");
+
+const input = document.querySelector("input");
+
+const todoList = document.getElementById("todos");
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const newTodo = {
+        "todo": input.value,
+        "completed": false
+    }
+
+    todos.push(newTodo);
+
+    saveTodos();
+
+    refreshTodos();
+
+    input.value = "";
+})
+
+function refreshTodos() {
+
+    todoList.innerHTML = "";
+
+    for (let i = 0; i < todos.length; i++) {
+
+        todoElement = document.createElement("li");
+
+        todoElement.setAttribute('onclick', `completeTodo(${i})`);
+
+        deleteButton = document.createElement("button");
+
+        deleteButton.classList.add('delete-btn', 'material-icons');
+
+        buttonText = document.createTextNode("delete");
+
+        deleteButton.appendChild(buttonText);
+
+        deleteButton.setAttribute('onclick', `deleteTodo(${i})`);
+
+        todoElement.innerText = todos[i].todo;
+
+        if (todos[i].completed) {
+            todoElement.classList.add('completed');
+        }
+
+        todoElement.appendChild(deleteButton);
+
+        todoList.appendChild(todoElement);
+    }
+
+}
+
+refreshTodos();
+
+function completeTodo(index) {
+
+    todos[index].completed = !todos[index].completed;
+
+    refreshTodos();
+
+    saveTodos();
+}
+
+function deleteTodo(index) {
+
+    event.stopPropagation(); // prevent the completeTodo function from being triggered when the user delete a todo
+
+    todos.splice(index, 1);
+
+    refreshTodos();
+
+    saveTodos();
+}
+
+
+function saveTodos() {
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+}
